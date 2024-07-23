278c068353eafb6890dd7dd8653c001604a83ee2
macdanniels <hoghidan1@gmail.com>
2024-07-23 10:17:06 +0100

    my first deployment1

--- AVCreg/create_db.py
+++ AVCreg/create_db.py
@@ -1,6 +1,9 @@
 from app import app, db 
 with app.app_context():
         try:
+            db.drop_all()
+            print("All tables dropped")
+            
             db.create_all()
             print("Database initialized successfully.")
         except Exception as e:
